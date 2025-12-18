82cdf68a1b6a32b015541245aa6c8453c0da7226
bala-amypo <bala@amypo.in>
2025-12-18 05:22:31 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/Sensor.java
@@ -0,0 +1,38 @@
+package com.example.demo.entity;
+
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+
+@Entity
+@Table(name = "sensors", uniqueConstraints = @UniqueConstraint(columnNames = "sensorCode"))
+public class Sensor {
+
+
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
+
+
+@Column(nullable = false, unique = true)
+private String sensorCode;
+
+
+@Column(nullable = false)
+private String sensorType;
+
+
+@ManyToOne
+@JoinColumn(name = "location_id", nullable = false)
+private Location location;
+
+
+private LocalDateTime installedAt = LocalDateTime.now();
+
+
+private Boolean isActive = true;
+
+
+
+}
