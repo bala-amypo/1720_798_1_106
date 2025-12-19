6ef7071660fda8b9465f4f4cfc2bc6e4027fad4f
bala-amypo <bala@amypo.in>
2025-12-19 08:33:26 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/LocationServiceimpl.java
@@ -0,0 +1,40 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Location;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.repository.LocationRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class LocationServiceimpl implements LocationService {
+
+    private final LocationRepository locationRepository;
+
+    // ✅ Constructor order EXACT
+    public LocationServiceImpl(LocationRepository locationRepository) {
+        this.locationRepository = locationRepository;
+    }
+
+    @Override
+    public Location createLocation(Location location) {
+
+        if (location.getRegion() == null || location.getRegion().isEmpty()) {
+            throw new IllegalArgumentException("region required");
+        }
+
+        return locationRepository.save(location);
+    }
+
+    @Override
+    public Location getLocation(Long id) {
+        return locationRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Location not found"));
+    }
+
+    @Override
+    public List<Location> getAllLocations() {
+        return locationRepository.findAll();
+    }
+}
