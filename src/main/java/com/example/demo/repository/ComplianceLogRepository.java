47205cc39d3ff60132282e9b3f63196c3f21b50d
bala-amypo <bala@amypo.in>
2025-12-25 15:29:10 +0000

    User commit

--- src/main/java/com/example/demo/repository/ComplianceLogRepository.java
+++ src/main/java/com/example/demo/repository/ComplianceLogRepository.java
@@ -8,5 +8,5 @@ import java.util.List;
 public interface ComplianceLogRepository
         extends JpaRepository<ComplianceLog, Long> {
 
-    List<ComplianceLog> findByReadingId(Long readingId);
+    List<ComplianceLog> findBySensorReading_Id(Long readingId);
 }
