1238f0bb70233f32828ee71f85b19edc928dbafb
bala-amypo <bala@amypo.in>
2025-12-25 15:15:21 +0000

    User commit

--- src/main/java/com/example/demo/service/LocationService.java
+++ src/main/java/com/example/demo/service/LocationService.java
@@ -1,13 +1,10 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.Location;
-import java.util.List;
 
 public interface LocationService {
 
     Location createLocation(Location location);
 
-    List<Location> getAllLocations();
-
-    Location getLocationById(Long id);
+    Location getLocation(Long id);
 }
--- src/main/java/com/example/demo/service/SensorService.java
+++ src/main/java/com/example/demo/service/SensorService.java
@@ -1,11 +1,10 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.Sensor;
-import java.util.List;
 
 public interface SensorService {
 
     Sensor createSensor(Long locationId, Sensor sensor);
 
-    List<Sensor> getAllSensors();   // 👈 this method exists
+    Sensor getSensor(Long id);
 }
