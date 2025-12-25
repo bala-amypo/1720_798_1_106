76543392dd1af7298630e98337d2c4999c0441e4
bala-amypo <bala@amypo.in>
2025-12-25 14:54:18 +0000

    User commit

--- src/main/java/com/example/demo/service/ComplianceEvaluationService.java
+++ src/main/java/com/example/demo/service/ComplianceEvaluationService.java
@@ -4,8 +4,6 @@ import com.example.demo.entity.ComplianceLog;
 import java.util.List;
 
 public interface ComplianceEvaluationService {
-
     ComplianceLog evaluateReading(Long readingId);
-
     List<ComplianceLog> getLogsByReading(Long readingId);
 }
