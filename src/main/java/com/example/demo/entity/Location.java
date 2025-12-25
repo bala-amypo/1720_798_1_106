5818348100a9fdeee256dbd1dde0f52043d25fc7
bala-amypo <bala@amypo.in>
2025-12-25 13:43:20 +0000

    User commit

--- src/main/java/com/example/demo/entity/Location.java
+++ src/main/java/com/example/demo/entity/Location.java
@@ -6,22 +6,11 @@ import jakarta.persistence.*;
 public class Location {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue
     private Long id;
 
-    private String name;
+    private String locationName;
+    private String region;
 
-    public Location() {}
-
-    public Location(String name) {
-        this.name = name;
-    }
-
-    public Long getId() { return id; }
-
-    public void setId(Long id) { this.id = id; }
-
-    public String getName() { return name; }
-
-    public void setName(String name) { this.name = name; }
+    // getters & setters
 }
