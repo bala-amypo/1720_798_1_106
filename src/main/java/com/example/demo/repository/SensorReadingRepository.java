ee3f98ddf8c842574b76260dc365f8945232f412
bala-amypo <bala@amypo.in>
2025-12-25 14:56:26 +0000

    User commit

--- src/main/java/com/example/demo/repository/SensorReadingRepository.java
+++ src/main/java/com/example/demo/repository/SensorReadingRepository.java
@@ -1,9 +1,4 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.SensorReading;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface SensorReadingRepository extends JpaRepository<SensorReading, Long> {
-    List<SensorReading> findBySensorId(Long sensorId);
+public interface SensorReadingRepository
+        extends JpaRepository<SensorReading, Long> {
+    List<SensorReading> findBySensor_Id(Long id);
 }
