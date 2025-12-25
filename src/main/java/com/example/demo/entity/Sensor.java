44d048626009924918c9612a6f0da07a019a1957
bala-amypo <bala@amypo.in>
2025-12-25 14:59:09 +0000

    User commit

--- src/main/java/com/example/demo/entity/Sensor.java
+++ src/main/java/com/example/demo/entity/Sensor.java
@@ -6,53 +6,36 @@ import jakarta.persistence.*;
 public class Sensor {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(unique = true)
     private String sensorCode;
 
     private String sensorType;
-
     private Boolean isActive = true;
 
     @ManyToOne
     private Location location;
 
-    // ===== GETTERS & SETTERS =====
+    public Long getId() { return id; }
 
-    public Long getId() {
-        return id;
-    }
-
-    public String getSensorCode() {
-        return sensorCode;
-    }
-
+    public String getSensorCode() { return sensorCode; }
     public void setSensorCode(String sensorCode) {
         this.sensorCode = sensorCode;
     }
 
-    public String getSensorType() {
-        return sensorType;
-    }
-
+    public String getSensorType() { return sensorType; }
     public void setSensorType(String sensorType) {
         this.sensorType = sensorType;
     }
 
-    public Boolean getIsActive() {
-        return isActive;
-    }
-
+    public Boolean getIsActive() { return isActive; }
     public void setIsActive(Boolean active) {
         isActive = active;
     }
 
-    public Location getLocation() {
-        return location;
-    }
-
+    public Location getLocation() { return location; }
     public void setLocation(Location location) {
         this.location = location;
     }
