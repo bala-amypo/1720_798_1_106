df4754e04837c58dd7b183c8f4abed33705f340d
bala-amypo <bala@amypo.in>
2025-12-26 03:40:17 +0000

    User commit

--- src/main/java/com/example/demo/service/SensorReadingService.java
+++ src/main/java/com/example/demo/service/SensorReadingService.java
@@ -3,8 +3,5 @@ package com.example.demo.service;
 import com.example.demo.entity.SensorReading;
 
 public interface SensorReadingService {
-
-    SensorReading submitReading(Long sensorId, SensorReading reading);
-
-    SensorReading getReading(Long id);
+    SensorReading save(SensorReading reading);
 }
