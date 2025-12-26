0a82bc64363902e7c6fd7a05e83e088cc691e3e8
bala-amypo <bala@amypo.in>
2025-12-26 03:40:00 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/SensorReadingServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/SensorReadingServiceImpl.java
@@ -1,7 +1,21 @@
-package com.example.demo.service;
+package com.example.demo.service.impl;
 
 import com.example.demo.entity.SensorReading;
+import com.example.demo.repository.SensorReadingRepository;
+import com.example.demo.service.SensorReadingService;
+import org.springframework.stereotype.Service;
 
-public interface SensorReadingService {
-    SensorReading save(SensorReading reading);
+@Service
+public class SensorReadingServiceImpl implements SensorReadingService {
+
+    private final SensorReadingRepository repository;
+
+    public SensorReadingServiceImpl(SensorReadingRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public SensorReading save(SensorReading reading) {
+        return repository.save(reading);
+    }
 }
