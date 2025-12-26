e261382a5630de8c08081099b6256ec5b1bf3dcb
bala-amypo <bala@amypo.in>
2025-12-26 03:40:55 +0000

    User commit

--- src/main/java/com/example/demo/controller/SensorController.java
+++ src/main/java/com/example/demo/controller/SensorController.java
@@ -4,24 +4,25 @@ import com.example.demo.entity.Sensor;
 import com.example.demo.service.SensorService;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/sensors")
 public class SensorController {
 
-    private final SensorService sensorService;
+    private final SensorService service;
 
-    public SensorController(SensorService sensorService) {
-        this.sensorService = sensorService;
+    public SensorController(SensorService service) {
+        this.service = service;
     }
 
-    @PostMapping("/{locationId}")
-    public Sensor createSensor(@PathVariable Long locationId,
-                               @RequestBody Sensor sensor) {
-        return sensorService.createSensor(locationId, sensor);
+    @PostMapping
+    public Sensor create(@RequestBody Sensor sensor) {
+        return service.create(sensor);
     }
 
-    @GetMapping("/{id}")
-    public Sensor getSensor(@PathVariable Long id) {
-        return sensorService.getSensor(id);
+    @GetMapping
+    public List<Sensor> getAll() {
+        return service.getAll();
     }
 }
