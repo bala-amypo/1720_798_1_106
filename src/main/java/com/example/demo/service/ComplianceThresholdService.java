8b06753094cae0604b9f22f783010876dfe76272
bala-amypo <bala@amypo.in>
2025-12-26 05:28:01 +0000

    User commit

--- src/main/java/com/example/demo/service/ComplianceThresholdService.java
+++ src/main/java/com/example/demo/service/ComplianceThresholdService.java
@@ -3,8 +3,6 @@ package com.example.demo.service;
 import com.example.demo.entity.ComplianceThreshold;
 
 public interface ComplianceThresholdService {
-
     ComplianceThreshold createThreshold(ComplianceThreshold threshold);
-
     ComplianceThreshold getThresholdBySensorType(String sensorType);
 }
